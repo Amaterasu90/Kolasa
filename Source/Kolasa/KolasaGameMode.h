f2bfb5af64f5d288d3753d52c51d5eb80c263e44
Ciastek90 <arekpoly@gmail.com>
2016-01-21 15:50:01 +0100

    Initialize project

---
+++ Source/Kolasa/KolasaGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "KolasaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KOLASA_API AKolasaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
