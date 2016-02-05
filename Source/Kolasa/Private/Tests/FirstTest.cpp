787fe4886bd440f501f98f1e71fa1d3bde3102d9
Ciastek90 <arekpoly@gmail.com>
2016-02-05 15:41:21 +0100

    Test check name of BoomCamera

--- Source/Kolasa/Private/Tests/FirstTest.cpp
+++ Source/Kolasa/Private/Tests/FirstTest.cpp
@@ -1,4 +1,7 @@
 #include "Kolasa.h"
+#include "Characters/MyCharacter.h"
+#include "Engine.h"
+#include "AutomationCommon.h" //To get the wait latent command.
 #include "AutomationTest.h"
 
 IMPLEMENT_SIMPLE_AUTOMATION_TEST(FFloatTest, "UnitTests.Equal.Float", (EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::SmokeFilter))
@@ -14,5 +17,35 @@ IMPLEMENT_SIMPLE_AUTOMATION_TEST(FIntTest, "UnitTests.Equal.Int", (EAutomationTe
 bool FIntTest::RunTest(const FString& Parameters)
 {
 	TestEqual<int>("Float equeal", 1,2);
+	return true;
+}
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FACharacter_InitializeSpringArmComponent_DefaultConstructor_BoomCameraNameBoomCameraTest, "Characer.UnitTests",
+	(EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::SmokeFilter))
+
+UWorld* GetWorld()
+{
+	if (GEngine)
+	{
+		if (GEngine->GetWorldContexts().Num() == 1)
+		{
+			return GEngine->GetWorldContexts()[0].World();
+		}
+	}
+	return nullptr;
+}
+
+bool FACharacter_InitializeSpringArmComponent_DefaultConstructor_BoomCameraNameBoomCameraTest::RunTest(const FString& Parameters) {
+	UWorld* world = GetWorld();
+	if (world)
+	{
+		FString expected = FString(TEXT("BoomCamera1"));
+		FString actual = FString();
+
+		AMyCharacter* character = world->SpawnActor<AMyCharacter>(AMyCharacter::StaticClass());
+
+		actual = character->BoomCamera->GetName();
+		TestEqual<FString>("SpringArm name equal", expected, actual);
+	}
 	return true;
 }
